c68d80050e168c9c863718ce5f8e82f67a2c191b
sandrinebedard <sandrine.bedard@polymtl.ca>
2022-06-22 17:00:20 -0400

    upadte comments & clean code

--- process_data.sh
+++ process_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Process data of different neck positions.
+# Process data of different neck positions (extension, flexion and straight).
 #
 # Usage:
 #   ./process_data.sh <SUBJECT>
-#
-# Manual segmentations or labels should be located under:
+# 
+# Manual segmentations and labels (discs, PMJ, nerve rootlets) should be located under:
 # PATH_DATA/derivatives/labels/SUBJECT/anat/
 #
 # Authors: Sandrine Bédard
@@ -31,7 +31,7 @@ start=`date +%s`
 
 # Check if manual label already exists. If it does, copy it locally. If it does
 # not, perform labeling.
-# NOTE: manual disc labels include C1-2, C2-3 and C3-4.
+# NOTE: manual disc labels should go from C1-C2 to C7-T1.
 label_if_does_not_exist(){
   local file="$1"
   local file_seg="$2"
@@ -78,7 +78,7 @@ detect_pmj_if_does_not_exist(){
   local file="$1"
   local file_seg="$2"
   # Update global variable with segmentation file name
-  FILELABEL="${file}_labels-pmj"  # MAYBE TO CHANGE
+  FILELABEL="${file}_labels-pmj"
   FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}-manual.nii.gz"
   echo "Looking for manual label: $FILELABELMANUAL"
   if [[ -e $FILELABELMANUAL ]]; then
@@ -90,7 +90,6 @@ detect_pmj_if_does_not_exist(){
     echo "Not found. Proceeding with automatic labeling."
     # Detect PMJ
     sct_detect_pmj -i ${file}.nii.gz -c t2 -qc ${PATH_QC}
-    #sct_detect_pmj -i ${file}.nii.gz -c t2 -s ${file_seg}.nii.gz -qc ${PATH_QC}
 
   fi
 }
@@ -108,19 +107,20 @@ cd ${PATH_DATA_PROCESSED}
 mkdir -p ${SUBJECT}
 rsync -avzh $PATH_DATA/$SUBJECT/ ${SUBJECT}
 # Go to anat folder where all structural data are located
-echo $PWD
 cd ${SUBJECT}/anat/
 
 file_t2="${SUBJECT_ID}_${SES}_T2w"
 
+# Reorient to RPI
 sct_image -i ${file_t2}.nii.gz -setorient RPI -o ${file_t2}.nii.gz
 
 # Segment spinal cord (only if it does not exist)
 segment_if_does_not_exist $file_t2 "t2"
 file_t2_seg=$FILESEG
 
-# Create labeled segmentation (only if it does not exist) 
+# Create labeled segmentation of vertebral levels (only if it does not exist) 
 label_if_does_not_exist ${file_t2} ${file_t2_seg}
+# Rename labeled segmentation and disc labels to differentiate with spinal levels (further down)
 mv "${file_t2_seg}_labeled.nii.gz" "${file_t2_seg}_labeled_vert.nii.gz"
 mv "${file_t2_seg}_labeled_discs.nii.gz" "${file_t2_seg}_labeled_discs_vert.nii.gz"
 file_t2_seg_labeled="${file_t2_seg}_labeled_vert"
@@ -130,37 +130,45 @@ sct_qc -i ${file_t2}.nii.gz -s ${file_t2_seg_labeled}.nii.gz -p sct_label_verteb
 # Flatten scan along R-L direction (to make nice figures)
 sct_flatten_sagittal -i ${file_t2}.nii.gz -s ${file_t2_seg}.nii.gz
 
-
 # Detect PMJ
 detect_pmj_if_does_not_exist $file_t2 $file_t2_seg
 
-# Compute average cord CSA  at spinal levels
+# Retereive nerve rootlet manual labels
 file_nerve="${PATH_DATA}/derivatives/labels/${SUBJECT}/${folder_contrast}/${file_t2}_labels-spinalroots-manual"
+if [[ -e $file_nerve ]]; then
+  echo "Found! Using manual nerve rootlets labels."
+
+else
+  echo "Not found. Please provide manual nerve rootlet label using identify_nerve.sh."
+
+fi
+
 # Create a labeled segmentation with spinal levels
 sct_label_vertebrae -i ${file_t2}.nii.gz -s ${file_t2_seg}.nii.gz -discfile ${file_nerve}.nii.gz -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT}
 file_t2_seg_labeled_nerve="${file_t2_seg}_labeled"
 
 # Get z slices of nerve labels
 slices_nerves=($(python $PATH_SCRIPT/get_disc_slice.py -label ${file_t2_seg}_labeled_discs.nii.gz -o ${PATH_RESULTS}))
+# Compute average cord CSA  at spinal levels
 for slices_nerv in "${slices_nerves[@]}";do
   sct_process_segmentation -i ${file_t2_seg}.nii.gz -z $slices_nerv -o ${PATH_RESULTS}/${SUBJECT_ID}_${SES}_csa-SC_spinal.csv -append 1
 done
 
-# Compute distance between PMJ and intervertebral discs
-# Run st_process_segmentation to generate centerline
+# Run sct_process_segmentation to generate centerline
 sct_process_segmentation -i ${file_t2_seg}.nii.gz -pmj ${file_t2}_pmj.nii.gz -pmj-distance 50 -pmj-extent 2 -o ${PATH_RESULTS}/${SUBJECT_ID}_${SES}_csa-50-pmj.csv -append 1 -qc ${PATH_QC} -qc-subject ${SUBJECT} -qc-image ${file_t2}.nii.gz -v 2
+# Compute distance between PMJ and intervertebral discs to use as PMJ-based CSA distance.
 python $PATH_SCRIPT/get_distance_pmj_disc.py -centerline ${file_t2_seg}_centerline_extrapolated.csv -disclabel ${file_t2_seg}_labeled_discs_vert.nii.gz -o ${PATH_RESULTS}/disc_pmj_distance.csv -spinalroots ${file_t2_seg}_labeled_discs.nii.gz -subject ${SUBJECT_ID}_${SES}
 
-# Compute average cord CSA between
 # Get slices to compute vertebral CSA
 slices_vert=($(python $PATH_SCRIPT/get_disc_slice.py -label ${file_t2_seg}_labeled_discs_vert.nii.gz -o ${PATH_RESULTS}))
+# Compute average cord CSA at every levels with 3 slices extent
 for slices in "${slices_vert[@]}";do
   sct_process_segmentation -i ${file_t2_seg}.nii.gz -z $slices -o ${PATH_RESULTS}/${SUBJECT_ID}_${SES}_csa-SC_vert.csv -append 1
 done
 
-# Compute average cord CSA at distance from each nerve from PMJ
-# Get z slices of vertebral labels
+# Get z slices of disc labels
 dist_nerves_pmj=($(python $PATH_SCRIPT/get_mean_nerve_dist.py -file-distance ${PATH_SCRIPT}/disc_pmj_distance_to_use.csv -subject ${SUBJECT_ID}))
+# Compute average cord CSA at distance from each nerve from PMJ with a 3 slice extent (or 2 mm) # TODO modify for 2.4
 for dist_nerv in "${dist_nerves_pmj[@]}";do
   sct_process_segmentation -i ${file_t2_seg}.nii.gz -pmj ${file_t2}_pmj.nii.gz -pmj-distance $dist_nerv -pmj-extent 2 -o ${PATH_RESULTS}/${SUBJECT_ID}_${SES}_csa-SC_pmj.csv -append 1 -qc ${PATH_QC} -qc-subject ${SUBJECT} -qc-image ${file_t2}.nii.gz -v 2
 done
@@ -170,8 +178,9 @@ done
 # ------------------------------------------------------------------------------
 FILES_TO_CHECK=(
   "${SUBJECT_ID}_${SES}_T2w_seg.nii.gz" 
-  "${SUBJECT_ID}_${SES}_T2w_seg_labeled.nii.gz"
-  "${SUBJECT_ID}_${SES}_T2w_labels-spinalroots-manual.nii.gz"
+  "${SUBJECT_ID}_${SES}_T2w_seg_centerline_extrapolated.csv"
+  "${SUBJECT_ID}_${SES}_T2w_labeled_discs.nii.gz"  # Spinal rootlet labels
+  "${SUBJECT_ID}_${SES}_T2w_labeled_discs_vert.nii.gz"  # Disc labels
 )
 pwd
 for file in ${FILES_TO_CHECK[@]}; do
